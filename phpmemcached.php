2e4a539b9dfc4b9bdde3f17d28bb98bb2e71fab1
xuyongwei <1041326797@qq.com>
2017-12-11 21:50:17 +0800

    xyw

--- phpmemcached.php
+++ phpmemcached.php
@@ -15,7 +15,7 @@ $tmp_object = new stdClass;
 $tmp_object->str_attr = 'test';
 $tmp_object->int_attr = 123;
 
-//$memcache->set('key', $tmp_object, false, 10) or die ("Failed to save data at the server");
+$memcache->set('key', $tmp_object, false, 10) or die ("Failed to save data at the server");
 echo "Store data in the cache (data will expire in 10 seconds  缓存在十秒钟后删除)<br/>\n";
 
 $get_result = $memcache->get('key');
